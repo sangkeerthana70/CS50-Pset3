006a856dcb22240b352f39a48306ba389e3a72c8
Anuradha <sangkeerthana70@gmail.com>
2018-04-24 03:50:09 +0000

    Completed Pset3

--- music/helpers.c
+++ music/helpers.c
@@ -4,6 +4,7 @@
 #include <string.h>//includes strcmp()
 #include <stdlib.h>
 #include <math.h>//pow()
+#include <ctype.h> //toupper
 
 #include "helpers.h"
 
@@ -11,16 +12,15 @@
 int duration(string fraction)
 {
     // TODO
-    //printf("fraction: %s\n", fraction);
 
     int numerator = fraction[0] - '0';
     //printf("numerator%i\n", numerator);
 
     int denominator = fraction[2] - '0';
-     //printf("denominator%i\n", denominator);
+    //printf("denominator%i\n", denominator);
 
     //calculate how many 1/8ths
-    int result = numerator * (8/(denominator));
+    int result = numerator * (8 / (denominator));
     //printf("result: %i\n", result);
     return result;
 }
@@ -29,78 +29,61 @@ int duration(string fraction)
 int frequency(string note)
 {
     //  TODO
-    // const string NOTES[] = {"C", "C#", "D", "D#", "E", "F",
-    //                     "F#", "G", "G#", "A", "A#", "B"
-    //                   };
-    int n = 0;
-    char inNote = note[0];
-    char oct = note[1];
-    switch(note[0]) {
-        case 'C' :
-            n = -9;
-            break;
-        case 'C#' :
-            //sscanf(note, "%1c%1i", &Note, &Octave);
-            n = -8;
-            break;
-        case 'D' :
-            n = -7;
-            break;
-        case 'D#' :
-            n = -6;
-            break;
-        case 'E' :
-            n = -5;
-            break;
-        case 'F' :
-            n = -4;
-            break;
-        case 'F#' :
-            n = -3;
-            break;
-        case 'G' :
-            n = -2;
-            break;
-        case 'G#' :
-            n = -1;
-            break;
-        case 'A#' :
-            n = 1;
-            break;
-        case 'B' :
-             n = 2;
-          default :
-            printf("n is:  %i\n",n);
-            return 0;
-       }
+    const string NOTES[] = {"C", "C#", "D", "D#", "E", "F",
+                            "F#", "G", "G#", "A", "A#", "B"
+                           };
+    char notes[3];
+    int oct;
+    bool flat = false;
 
-
-        if (strlen(note) == 2)
+    if (strlen(note) == 2)
+    {
+        notes[0] = toupper(note[0]);
+        notes[1] =  '\0';
+        oct = note[1] - '0';
+    }
+    else
+    {
+        notes[0] = toupper(note[0]);
+        if (note[1] == 'b')
         {
-
-            printf("Note%s\n", note);
-            printf("FirstLetter: %c\n", inNote);
-            printf("SecondLetter: %c\n", oct);
-
-            double baseNote = 440.00;//relates to AS
-            int semitone = 2 * pow(n, 12);
-            printf("%g\n", semitone);//g is placeholder for double
-            int freq = semitone * baseNote;
-
+            flat = true;
+            notes[1] =  '\0';
         }
-
         else
         {
-            char inNote = note[0];
-            char acc = note[1];
-            char oct = note[2];
-            printf("FirstLetter: %c\n", inNote);
-            printf("SecondLetter: %c\n", acc);
-            printf("ThirdLetter: %c\n", oct);
+            notes[1] = toupper(note[1]);
+            notes[2] =  '\0';
         }
-          return 0;
+        oct = note[2] - '0';
     }
-    return 0;
+    //printf("notes :%s\n",notes);
+    //printf("oct : %i\n",oct);
+
+    int length = sizeof(NOTES) / sizeof(*NOTES); //length of array
+    int posA = 9;
+    double n = 0;
+    double frequency = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (!(strcmp(notes, NOTES[i])))
+        {
+            n = i - posA;//position relative to A
+            n = n + (oct - 4) * 12; //adjust for the current octave relative to octave 4
+            if (flat)
+            {
+                n = n - 1;//adjust for flat
+            }
+
+            frequency = round(440 * pow(2.00, n / 12.00));
+            //printf("%f\n");
+        }
+    }
+    return frequency;
+}
+
+
+
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
